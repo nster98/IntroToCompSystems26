33fb93f12802f847c491bce081ed400450e0b89f
nster98 <nster98@gmail.com>
2020-03-10 14:14:43 -0400

    Added more code for searching

--- Proj1_IPC/problem2/search.c
+++ Proj1_IPC/problem2/search.c
@@ -18,12 +18,57 @@ int search(int* arr, int size, int factor, int key)
 	int status;
 	int sectionOfChild[factor];
 	pid_t pid[factor];
+
+	unsigned char exit_code;
 		
+	int num;
 	// Create a number of forks equal to size
 	for (int i = 0; i < size; i += amount)
 	{
-		//pid[i / amount] = fork();
-		
+		pid[i / amount] = fork();
+		num = i;
+
+		if (pid[num / amount] < 0) // Error
+		{
+			fprintf(stderr, "Failed to fork\n");
+			exit(-1);
+		}
+
+		if (pid[num / amount] == 0) // Child
+		{
+
+			int foundFlag = 0;
+
+			int end = (num + amount < size) ? num + amount : size;
+
+			for (int j = num; j < end; j++)
+			{
+				if (arr[j] == key)
+				{
+					foundFlag = 1;
+					exit(j % amount);
+				}
+			}
+			if (foundFlag == 0)
+			{
+				exit(-1);
+			}
+		}
+		else // Parent
+		{
+			
+		}
+	}
+
+	for (int k = 0; k < amount; k++)
+	{
+		waitpid(pid[k], &status, 0);
+		sectionOfChild[k] = k * amount;
+
+		if (WIFEXITED(status))
+		{
+			exit_code = WEXITSTATUS(status);
+		}
 	}
 	
 	return 0;	
@@ -42,6 +87,16 @@ int main(int argc, char* argv[])
 	{
 		if (fscanf(file, "%d", &arr[i]) != 1)
 			break;
-		printf("arr[%d] = %d\n", i, arr[i]);
-	}	
+		//printf("arr[%d] = %d\n", i, arr[i]);
+	}
+
+	// Set the keys at each point in the array
+	arr[MAX_NUM / 4] = -50;
+	arr[MAX_NUM / 2] = -50;
+	arr[(3 * MAX_NUM) / 4] = -50; 
+
+	// THOUGHTS
+	//
+	// There needs to be something like an array for the process to put its found index for the key into
+	// They should also have a maximum that is sent to each process whenever one of them finds something larger
 }
